d949c83d7e4e346d091ebd9141509e080c3588f7
kajah <katharine.jiang9@gmail.com>
2017-08-09 22:57:40 -0700

    initial barebones ui

--- index.android.js
+++ index.android.js
@@ -6,48 +6,52 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    View,
+    TouchableHighlight
 } from 'react-native';
 
 export default class Pause extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.contentContainer}>
+                    <TouchableHighlight style={styles.buttonContainer}>
+                        <View style={styles.pause} title="Pause" />
+                    </TouchableHighlight>
+                </View>
+                <View style={styles.box} />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    contentContainer: {
+        flex: 1,
+    },
+    buttonContainer: {
+        marginTop: 200,
+    },
+    pause: {
+        backgroundColor: "#F57C00",
+        width: 200,
+        height: 200,
+        borderRadius: 200/2,
+    },
+    box: {
+        height: 100,
+        alignSelf: 'stretch',
+        backgroundColor: "#CE93D8",
+        marginTop: 200,
+    }
 });
 
 AppRegistry.registerComponent('Pause', () => Pause);
